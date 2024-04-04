e3459d6ac95c09978d300a7f236e96d86d27377c
ikow-g <muh.djatmieka@gmail.com>
2024-04-04 13:22:26 +0700

    test 1

---
+++ src/navigation/nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img
+            src="/docs/5.3/assets/brand/bootstrap-logo.svg"
+            alt="Logo"
+            width="30"
+            height="24"
+            class="d-inline-block align-text-top"
+          ></img>
+          Bootstrap
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
