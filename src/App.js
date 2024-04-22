4ad65cec638253dce6dd422c9a556abb9f99c348
ikow-g <muh.djatmieka@gmail.com>
2024-04-22 11:47:41 +0700

    main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './navigation/nav';
+import Main from './layout/main';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Nav />
+        <Main />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
