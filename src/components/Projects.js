e5755982ace484a8bbaa0df672e4e6208396e62e
ikow-g <muh.djatmieka@gmail.com>
2024-05-06 09:51:13 +0700

    update tab

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -90,7 +90,7 @@ export const Projects = () => {
                           })}
                         </Row>
                       </Tab.Pane>
-                      <Tab.Pane eventKey="section">
+                      <Tab.Pane eventKey="second">
                         <p>
                           Lorem ipsum dolor sit amet consectetur adipisicing
                           elit. Cumque quam, quod neque provident velit, rem
