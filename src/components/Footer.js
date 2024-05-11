902c6cf812e0b861675189d4bcb3aeac42fd3cef
ikow-g <muh.djatmieka@gmail.com>
2024-05-11 12:06:48 +0700

    update links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,14 +16,14 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#">
-                <img src={navIcon1} alt="Icon" />
+              <a href="https://www.linkedin.com/in/muh-rizal-sakti-djatmieka/">
+                <img src={navIcon1} alt="Icon1" />
               </a>
-              <a href="#">
-                <img src={navIcon2} alt="Icon" />
+              <a href="https://facebook.com/">
+                <img src={navIcon2} alt="Icon2" />
               </a>
-              <a href="#">
-                <img src={navIcon3} alt="Icon" />
+              <a href="https://instagram.com/">
+                <img src={navIcon3} alt="Icon3" />
               </a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
